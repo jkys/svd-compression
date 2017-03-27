3ad55bb78d0b9c0eed6dcb0a1c300745196ef0b4
Jonathan <keysj@wit.edu>
2017-03-27 11:16:02 -0400

    Refactoring

--- src/Runner.java
+++ src/Runner.java
@@ -9,12 +9,12 @@ import javax.imageio.ImageIO;
  */
 public class Runner {
   public static void main (String[] args) throws IOException {
-    BufferedImage image = ImageIO.read(new File("/Users/jonathankeys/code/school/linearalgebra/src/images/image1.jpg"));
+    BufferedImage image = ImageIO.read(new File("src/images/image1.jpg"));
 
     ImageVectors matrix = new ImageVectors(image);
     matrix.rotateImageClockwise();
     image = matrix.getImage();
 
-    ImageIO.write(image, "jpg", new File("/Users/jonathankeys/code/school/linearalgebra/src/images/imageUpdated.jpg"));
+    ImageIO.write(image, "jpg", new File("src/images/imageUpdated.jpg"));
   }
 }
