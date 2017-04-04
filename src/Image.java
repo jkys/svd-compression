13b1383ea5c34d2568a3d2c72aa7296f0d9b1d28
Jonathan <keysj@wit.edu>
2017-04-04 13:47:05 -0400

    Major changes

--- src/Image.java
+++ src/Image.java
@@ -1,4 +1,5 @@
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -15,6 +16,9 @@ public class Image {
 
   public Image (String url) throws IOException {
     setImage(url);
+  }
+
+  public Image () throws IOException {
   }
 
   public void setImage (String url) throws IOException {
@@ -44,7 +48,7 @@ public class Image {
     }
   }
 
-  private void createImageFromMatrix () {
+  public void createImageFromMatrix() {
     for (int i = 0; i < this.width; i++) {
       for (int j = 0; j < this.height; j++) {
         int val = (int) Math.round(this.imageMatrix[i][j]);
@@ -61,4 +65,15 @@ public class Image {
     this.image = image;
   }
 
+  public BufferedImage createImage (double[][] matrix) {
+    BufferedImage newImage = new BufferedImage(matrix[0].length, matrix.length, BufferedImage.TYPE_INT_RGB);
+    for (int i = 0; i < matrix[0].length; i++) {
+      for (int j = 0; j < matrix.length; j++) {
+        int val = (int) Math.round(matrix[j][i]);
+        newImage.setRGB(i, j, val);
+      }
+    }
+
+    return newImage;
+  }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.imageio.ImageIO;
@@ -11,7 +10,7 @@ import org.apache.commons.math.FunctionEvaluationException;
  *
  */
 public class Runner {
-  public static void main (String[] args) throws IOException, FunctionEvaluationException, ConvergenceException {
+  public static void main (String[] args) throws Exception {
     // Create matrix from image
     Image image = new Image("src/images/image4.jpg");
     double[][] imageMatrix = image.getImageMatrix();
@@ -19,9 +18,60 @@ public class Runner {
     double[][] twoByTwo = {{5, 5}, {-1, 7}};
     double[][] threeByTwo = {{5, 5, 7}, {-1, 7, 8}};
     double[][] fourByTwo = {{2, 4}, {1, 3}, {0, 0}, {0, 0}};
-    svdCommented(imageMatrix);
+    svdCommented(twoByTwo);
+
+    int rank = 100;
+
+    imageMatrix = svd(imageMatrix, rank);
+
+    image.setImageMatrix(imageMatrix);
+    image.createImageFromMatrix();
 
     ImageIO.write(image.getImage(), "jpg", new File("src/images/imageUpdated.jpg"));
+  }
+
+  private static double[][] rankedMatrixS (Matrix matrix, int rank) {
+    double[][] rankedMatrix = new double[rank][rank];
+    for (int i = 0; i < rank; i++) {
+      double[] rowMatrix = matrix.getMatrix()[i];
+      System.arraycopy(rowMatrix, 0, rankedMatrix[i], 0, rank);
+    }
+    return rankedMatrix;
+  }
+
+  private static double[][] rankedMatrixUV (Matrix matrix, int rank) {
+    int rows = matrix.getRowCount();
+    double[][] rankedMatrix = new double[rows][rank];
+    for (int i = 0; i < rows; i++) {
+      double[] rowMatrix = matrix.getMatrix()[i];
+      System.arraycopy(rowMatrix, 0, rankedMatrix[i], 0, rank);
+    }
+    return rankedMatrix;
+  }
+
+  private static double[][] svd (double[][] imageMatrix, int rank) throws Exception {
+    Jama.Matrix matrix = new Jama.Matrix(imageMatrix);
+
+    int maxRank = matrix.rank();
+
+    if (rank > maxRank) {
+      rank = maxRank;
+    }
+
+    Jama.Matrix S = matrix.svd().getS();
+    Jama.Matrix V = matrix.svd().getV();
+    Jama.Matrix U = matrix.svd().getU();
+
+    S = new Jama.Matrix(rankedMatrixS(new Matrix(S.getArray()), rank));
+    V = new Jama.Matrix(rankedMatrixUV(new Matrix(V.getArray()), rank));
+    U = new Jama.Matrix(rankedMatrixUV(new Matrix(U.getArray()), rank));
+
+    ImageIO.write(new Image().createImage(S.getArray()), "jpg", new File("src/images/S.jpg"));
+    ImageIO.write(new Image().createImage(V.getArray()), "jpg", new File("src/images/V.jpg"));
+    ImageIO.write(new Image().createImage(U.getArray()), "jpg", new File("src/images/U.jpg"));
+
+    Matrix composed = new Matrix(U.times(S).times(V.transpose()).getArray());
+    return composed.getMatrix();
   }
 
   private static void svdCommented (double[][] twoByTwo) throws FunctionEvaluationException, ConvergenceException {
@@ -29,14 +79,14 @@ public class Runner {
     Matrix transpose = matrix.transposeMatrix();
     Matrix square = transpose.multiplyMatrix(matrix);
 
-//    System.out.println("Matrix: Original");
-//    matrix.printMatrixTrim();
+    System.out.println("Matrix: Original");
+    matrix.printMatrixTrim();
 
-//    System.out.println("Matrix: Transposed");
-//    transpose.printMatrixTrim();
+    System.out.println("Matrix: Transposed");
+    transpose.printMatrixTrim();
 
-//    System.out.println("Matrix: Square");
-//    square.printMatrixTrim();
+    System.out.println("Matrix: Square");
+    square.printMatrixTrim();
 
     Jama.EigenvalueDecomposition r = new Jama.EigenvalueDecomposition(new Jama.Matrix(square.getMatrix()));
 
@@ -53,22 +103,22 @@ public class Runner {
 
     svd.createS(r.getRealEigenvalues(), matrix.getRowCount(), matrix.getColumnCount());
     Matrix S = new Matrix(svd.getS());
-//    System.out.println("Matrix: S");
-//    S.printMatrixTrim();
+    System.out.println("Matrix: S");
+    S.printMatrixTrim();
 
     svd.createV(eigenMatrices, matrix.getColumnCount());
     Matrix V = new Matrix(svd.getV());
-//    System.out.println("Matrix: V");
-//    V.printMatrixTrim();
+    System.out.println("Matrix: V");
+    V.printMatrixTrim();
 
     svd.createU(matrix);
     Matrix U = new Matrix(svd.getU());
-//    System.out.println("Matrix: U");
-//    U.printMatrixTrim();
+    System.out.println("Matrix: U");
+    U.printMatrixTrim();
 
 
     Matrix SUV = new Matrix(svd.compose());
-//    System.out.println("Matrix: Reconstructed");
-//    SUV.printMatrix();
+    System.out.println("Matrix: Reconstructed");
+    SUV.printMatrix();
   }
 }
