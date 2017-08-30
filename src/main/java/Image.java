6a2df8eba9f2548ab6450e4d603c29b82d9c9dc8
Jonathan <keysj@wit.edu>
2017-08-29 20:40:33 -0400

    Reformatted code to be with checkstyle

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -6,74 +6,74 @@ import javax.imageio.ImageIO;
 
 /**
  * Created by jonathankeys on 3/26/17.
- *
  */
 public class Image {
+
     private BufferedImage image;
     private double[][] imageMatrix;
     private int height;
     private int width;
 
-    public Image (String url) throws IOException {
-            setImage(url);
+    public Image(String url) throws IOException {
+        setImage(url);
     }
 
-    public Image () throws IOException {
+    public Image() throws IOException {
     }
 
-    public void setImage (String url) throws IOException {
-            this.image = ImageIO.read(new File(url));
+    public void setImage(String url) throws IOException {
+        this.image = ImageIO.read(new File(url));
     }
 
-    public double[][] getImageMatrix () {
-            createMatrixFromImage();
-            return this.imageMatrix;
+    public double[][] getImageMatrix() {
+        createMatrixFromImage();
+        return this.imageMatrix;
     }
 
-    public BufferedImage getImage () {
-            return this.image;
+    public BufferedImage getImage() {
+        return this.image;
     }
 
-    private void createMatrixFromImage () {
-            this.height = image.getHeight();
-            this.width = image.getWidth();
+    private void createMatrixFromImage() {
+        this.height = image.getHeight();
+        this.width = image.getWidth();
 
-            this.imageMatrix = new double[this.width][this.height];
+        this.imageMatrix = new double[this.width][this.height];
 
-            for (int i = 0; i < width; i++) {
-                    for (int j = 0; j < height; j++) {
-                            int rgb = image.getRGB(i, j);
-                            this.imageMatrix[i][j] = rgb;
-                    }
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int rgb = image.getRGB(i, j);
+                this.imageMatrix[i][j] = rgb;
             }
+        }
     }
 
     public void createImageFromMatrix() {
-            for (int i = 0; i < this.width; i++) {
-                    for (int j = 0; j < this.height; j++) {
-                            int val = (int) Math.round(this.imageMatrix[i][j]);
-                            this.image.setRGB(i, j, val);
-                    }
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                int val = (int) Math.round(this.imageMatrix[i][j]);
+                this.image.setRGB(i, j, val);
             }
+        }
     }
 
-    public void setImageMatrix (double[][] newImageMatrix) {
-            this.imageMatrix = newImageMatrix;
+    public void setImageMatrix(double[][] newImageMatrix) {
+        this.imageMatrix = newImageMatrix;
     }
 
-    public void setImage (BufferedImage image) {
-            this.image = image;
+    public void setImage(BufferedImage image) {
+        this.image = image;
     }
 
-    public BufferedImage createImage (double[][] matrix) {
-            BufferedImage newImage = new BufferedImage(matrix[0].length, matrix.length, BufferedImage.TYPE_INT_RGB);
-            for (int i = 0; i < matrix[0].length; i++) {
-                    for (int j = 0; j < matrix.length; j++) {
-                            int val = (int) Math.round(matrix[j][i]);
-                            newImage.setRGB(i, j, val);
-                    }
+    public BufferedImage createImage(double[][] matrix) {
+        BufferedImage newImage = new BufferedImage(matrix[0].length, matrix.length, BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                int val = (int) Math.round(matrix[j][i]);
+                newImage.setRGB(i, j, val);
             }
+        }
 
-            return newImage;
+        return newImage;
     }
 }
--- src/main/java/SVD.java
+++ src/main/java/SVD.java
@@ -2,88 +2,86 @@ import java.util.List;
 
 /**
  * Created by jonathankeys on 3/31/17.
- *
  */
 public class SVD {
+
     private double[][] S;
     private double[][] U;
     private double[][] V;
 
-    public SVD () {
+    public SVD() {
 
     }
 
     public Matrix compose() {
-            Matrix matrixS = new Matrix(S);
-            Matrix matrixU = new Matrix(U);
-            Matrix matrixV = new Matrix(V);
+        Matrix matrixS = new Matrix(S);
+        Matrix matrixU = new Matrix(U);
+        Matrix matrixV = new Matrix(V);
 
-            Matrix US = matrixU.multiplyMatrix(matrixS);
-            Matrix composed = US.multiplyMatrix(matrixV);
-            composed.convertToInt();
+        Matrix US = matrixU.multiplyMatrix(matrixS);
+        Matrix composed = US.multiplyMatrix(matrixV);
+        composed.convertToInt();
 
-            return composed;
+        return composed;
     }
 
-    public void createS (double[] eigenValues, int rows, int columns) {
-            for (int i = 0, eigenLength = eigenValues.length; i < eigenLength; i++) {
-                    eigenValues[i] = Math.sqrt(eigenValues[i]);
-            }
+    public void createS(double[] eigenValues, int rows, int columns) {
+        for (int i = 0, eigenLength = eigenValues.length; i < eigenLength; i++) {
+            eigenValues[i] = Math.sqrt(eigenValues[i]);
+        }
 
-            Matrix matrixS = new Matrix();
-            matrixS.createZeroIdentityFromVector(eigenValues, rows, columns);
-            setS(matrixS.getMatrix());
+        Matrix matrixS = new Matrix();
+        matrixS.createZeroIdentityFromVector(eigenValues, rows, columns);
+        setS(matrixS.getMatrix());
     }
 
-    public void createV (List<Matrix> eigenMatrices, int columns) {
+    public void createV(List<Matrix> eigenMatrices, int columns) {
 
-            new Jama.EigenvalueDecomposition(new Jama.Matrix(eigenMatrices.get(0).getMatrix())).getRealEigenvalues();
-            Matrix V = new Matrix(new Jama.Matrix(eigenMatrices.get(0).getMatrix()).eig().getV().getArray());
-            double[][] e = V.getMatrix();
-            int size = e.length;
-            for (int i = 0; i < e.length; i++) {
-                    double[] temp = e[i];
-                    e[i] = e[size - 1];
-                    e[size - 1] = temp;
-            }
+        new Jama.EigenvalueDecomposition(new Jama.Matrix(eigenMatrices.get(0).getMatrix())).getRealEigenvalues();
+        Matrix V = new Matrix(new Jama.Matrix(eigenMatrices.get(0).getMatrix()).eig().getV().getArray());
+        double[][] e = V.getMatrix();
+        int size = e.length;
+        for (int i = 0; i < e.length; i++) {
+            double[] temp = e[i];
+            e[i] = e[size - 1];
+            e[size - 1] = temp;
+        }
 
-
-
-            V = new Matrix(e);
-            V = V.transposeMatrix();
-            setV(V.getMatrix());
+        V = new Matrix(e);
+        V = V.transposeMatrix();
+        setV(V.getMatrix());
     }
 
-    public void  createU (Matrix matrix) {
-            Matrix US = new Matrix(matrix.multiplyMatrix(new Matrix(getV())));
+    public void createU(Matrix matrix) {
+        Matrix US = new Matrix(matrix.multiplyMatrix(new Matrix(getV())));
 
-            Matrix U = new Matrix();
-            U.createUnitMatrix(US);
+        Matrix U = new Matrix();
+        U.createUnitMatrix(US);
 
-            setU(U.getMatrix());
+        setU(U.getMatrix());
     }
 
-    public double[][] getS () {
-            return this.S;
+    public double[][] getS() {
+        return this.S;
     }
 
-    public double[][] getU () {
-            return this.U;
+    public double[][] getU() {
+        return this.U;
     }
 
-    public double[][] getV () {
-            return this.V;
+    public double[][] getV() {
+        return this.V;
     }
 
     private void setS(double[][] S) {
-            this.S = S;
+        this.S = S;
     }
 
     private void setU(double[][] U) {
-            this.U = U;
+        this.U = U;
     }
 
     private void setV(double[][] V) {
-            this.V = V;
+        this.V = V;
     }
 }
