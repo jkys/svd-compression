8daee6a9b6e0831b4bf14855d2bf20460ca5fc2e
Jonathan <keysj@wit.edu>
2017-09-09 11:28:15 -0400

    Added much more documentation to the Image class

--- src/main/java/util/Image.java
+++ src/main/java/util/Image.java
@@ -18,6 +18,11 @@ public class Image {
     private int height;
     private int width;
 
+    /**
+     * Constructor method to set image.
+     * @param url file path of the image to be created.
+     * @throws IOException
+     */
     public Image(String url) throws IOException {
         setImage(url);
     }
@@ -25,19 +30,35 @@ public class Image {
     public Image() throws IOException {
     }
 
+    /**
+     * Sets an image by reading in the file url and sets that to the class BufferedImage object
+     * @param url file path of the image on local computer
+     * @throws IOException
+     */
     private void setImage(String url) throws IOException {
         this.image = ImageIO.read(new File(url));
     }
 
+    /**
+     * Accessor method for the matrix of the BufferedImage
+     * @return double[][] containing all the values of the matrix in a double multi-dimensional array
+     */
     public double[][] getImageMatrix() {
         createMatrixFromImage();
         return this.imageMatrix;
     }
 
+    /**
+     * Accessor method to retrieve the image as an image
+     * @return BufferedImage object of the matrix itself
+     */
     public BufferedImage getImage() {
         return this.image;
     }
 
+    /**
+     * Sets the image from a BufferedImage to a double[][] image
+     */
     private void createMatrixFromImage() {
         this.height = image.getHeight();
         this.width = image.getWidth();
@@ -52,6 +73,9 @@ public class Image {
         }
     }
 
+    /**
+     * Recreate an image based on a double matrix and set it to a BufferedImage
+     */
     public void createImageFromMatrix() {
         for (int i = 0; i < this.width; i++) {
             for (int j = 0; j < this.height; j++) {
@@ -61,10 +85,19 @@ public class Image {
         }
     }
 
+    /**
+     * Setter method to set the imageMatrix of the class object to be used in creating BufferedImages
+     * @param newImageMatrix double[][] matrix to be set to class object
+     */
     public void setImageMatrix(double[][] newImageMatrix) {
         this.imageMatrix = newImageMatrix;
     }
 
+    /**
+     * Create an image based on a double[][] matrix of values
+     * @param matrix double[][] matrix to be used to converted into a BufferedImage
+     * @return BufferedImage representation of the double[][] matrix
+     */
     public BufferedImage createImage(double[][] matrix) {
         BufferedImage newImage = new BufferedImage(matrix[0].length, matrix.length, BufferedImage.TYPE_INT_RGB);
         for (int i = 0; i < matrix[0].length; i++) {
